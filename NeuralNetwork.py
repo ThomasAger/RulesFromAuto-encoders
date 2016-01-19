120400f2381cd818f36a1e02053f26848d9bce3e
Thomas Ager <tom@eygrr.com>
2016-01-19 19:38:47 +0000

    Made the Neural Network code work with the new beans. Removed old stuff.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import importdata
-import outputdata
+
 import numpy as np
 
+import DataTasks as dt
+import MovieTasks
 from keras.layers.core import Dense, Activation, Dropout
 from keras.optimizers import SGD, Adagrad, Adadelta, Adam
 import theano
@@ -31,22 +32,27 @@ class NeuralNetwork:
 
 
 
-    def __init__(self, input_size=200, hidden_size=400, output_size=100, training_data=10000, class_type="Genres",
+    def __init__(self, input_size=200, hidden_size=[400], output_size=100, training_data=10000, class_type="Genres",
                  epochs=250,  learn_rate=0.01, loss="binary_crossentropy", hidden_amount=1, batch_size=100, decay=1e-06,
                  hidden_activation="tanh", layer_init="glorot_uniform", output_activation="sigmoid", dropout_chance=0.5, hidden_layer_space=1,
                  class_mode="binary", save_weights=False, save_space=False, save_architecture=False, file_name="unspecified_filename", vector_path=None):
 
 
-        movie_names, movie_vectors, movie_labels = self.importData(class_type, input_size, vector_path)
-        n_train, x_train, y_train, n_test, x_test, y_test = self.splitData(training_data, movie_names, movie_vectors, movie_labels)
-
+        movie_names, movie_vectors, movie_labels = MovieTasks.getMovieData(class_type=class_type, class_names=None, input_size=input_size, vector_path=vector_path)
+        n_train, x_train, y_train, n_test, x_test, y_test = dt.splitData(training_data, movie_names, movie_vectors, movie_labels)
+        n_train = np.asarray(n_train)
+        x_train = np.asarray(x_train)
+        y_train = np.asarray(y_train)
+        n_test = np.array(n_test)
+        x_test = np.asarray(x_test)
+        y_test = np.asarray(y_test)
         self.model = Sequential()
         self.addLayers(self.model, hidden_amount, dropout_chance, hidden_activation, layer_init, hidden_size, input_size, output_size, output_activation)
         self.predicted_classes = self.trainNetwork(self.model, x_train, y_train, x_test, y_test, epochs, batch_size, learn_rate, decay, loss, class_mode)
 
         self.model_architecture = self.model.to_json()
         self.f1_score = f1_score(y_test, self.predicted_classes, average='macro')
-        self.correct_classes = self.printClasses(y_test, self.predicted_classes, n_test)
+        self.correct_classes = dt.printClasses(y_test, self.predicted_classes, n_test)
 
 
         if save_architecture == True:
@@ -55,41 +61,15 @@ class NeuralNetwork:
         if save_weights == True:
             self.model.save_weights("newdata/weights/" + file_name + ".h5", overwrite=True)
         if save_space == True:
-            self.saveSpace(self.model, movie_vectors, hidden_layer_space, "newdata/spaces/" + file_name + ".mds")
-
-
-    def importData(self, class_type="All", input_size=200, vector_path=None):
-        movie_names = importdata.importString("filmdata/filmNames.txt")
-
-        if class_type == "All":
-            movie_labels = importdata.getAllLabels("Keywords")
-            genre_labels = importdata.getAllLabels("Genres")
-            for c in range(len(movie_labels)):
-                movie_labels[c].extend(genre_labels[c])
-        else: movie_labels = importdata.getAllLabels(class_type)
-
-        if vector_path is None:
-            movie_vectors = importdata.getMovieVectors(input_size)
-        else:
-            movie_vectors = importdata.getMovieVectors(input_size, vector_path)
-
-
-        return movie_names, movie_vectors, movie_labels
-
-
-    def splitData(self, training_data, movie_names, movie_vectors, movie_labels):
-        n_train = movie_names[:training_data]
-        x_train = np.asarray(movie_vectors[:training_data])
-        y_train = np.asarray(movie_labels[:training_data])
-
-        n_test = movie_names[training_data:]
-        x_test = np.asarray(movie_vectors[training_data:])
-        y_test = np.asarray(movie_labels[training_data:])
-        return n_train, x_train, y_train, n_test, x_test, y_test
+            if hidden_amount > 1:
+                for i in range(hidden_amount - 1):
+                    self.saveSpace(self.model, movie_vectors, i, "newdata/spaces/" + file_name + str(hidden_amount) +".mds")
+            else:
+                self.saveSpace(self.model, movie_vectors, hidden_amount, "newdata/spaces/" + file_name + ".mds")
 
     def addLayers(self, model,  hidden_amount, dropout_chance, hidden_activation, layer_init, hidden_size, input_size, output_size, output_activation):
 
-        model.add(Dense(output_dim=hidden_size, input_dim=input_size, init=layer_init))
+        model.add(Dense(output_dim=hidden_size[0], input_dim=input_size, init=layer_init))
 
         for x in range(hidden_amount):
             model.add(Activation(hidden_activation))
@@ -108,121 +88,42 @@ class NeuralNetwork:
 
 
     def saveSpace(self, model, movie_vectors, layer, file_name):
-        hidden_layer = theano.function([model.layers[0].input], model.layers[layer].get_output(train=False))
+
+
+        # When using a Dense as the hidden layer, this fails as it apparently has no attribute 'Output'
+        hidden_layer = theano.function([model.layers[0].input], model.layers[1].get_output(train=False), allow_input_downcast=True)
+        print hidden_layer
         transformed_space = hidden_layer(np.asarray(movie_vectors, dtype=np.float32))
 
-        outputdata.write2dArray(transformed_space, file_name)
+        dt.write2dArray(transformed_space, file_name)
 
-    # refactor this
-    def printClasses(self, Y_test, Y_predicted, n_test):
-        misclassified1 = 0
-        classified1 = 0
-        misclassified0 = 0
-        classified0 = 0
-        correctCount = 0
-        amt_correct = 0
-        films_classified_correctly = []
-        for sets in range(len(Y_predicted)):
-            correctCount = 0
-            for c in range(len(Y_predicted[sets])):
-                if Y_test[sets][c] == 1 and Y_predicted[sets][c] == 0:
-                    misclassified1 = misclassified1 + 1
-                elif Y_test[sets][c] == 1 and Y_predicted[sets][c] == 1:
-                    classified1 = classified1 + 1
-                    correctCount = correctCount + 1
-                elif Y_test[sets][c] == 0 and Y_predicted[sets][c] == 1:
-                    misclassified0 = misclassified0 + 1
-                else:
-                    classified0 = classified0 + 1
-                    correctCount = correctCount + 1
-            if correctCount >= len(Y_test[0]):
-                amt_correct = amt_correct + 1
 
-        return amt_correct
 
 def main():
 
-    print "#### layer sizes ####"
-    """
+    print "ALL"
     class_type = "All"
     output_size = 125
     loss = "sparse_bias_binary_crossentropy"
 
-    dimension_200 = NeuralNetwork(save_weights=True, epochs=250,
-                                 hidden_size=400, hidden_amount=2, output_size=output_size, class_type=class_type, loss=loss, save_space=True, save_architecture=True, file_name="All-Layer-standard" + str(2))
     dimension_100 = NeuralNetwork(save_weights=True, epochs=250,
-                                 hidden_size=400, hidden_amount=4, output_size=output_size, class_type=class_type, loss=loss, save_space=True, save_architecture=True, file_name="All-Layer-standard" + str(4))
-    dimension_50 = NeuralNetwork( save_weights=True, epochs=250,
-                                 hidden_size=400, hidden_amount=8, output_size=output_size, class_type=class_type, loss=loss, save_space=True, save_architecture=True, file_name="All-Layer-standard" + str(8))
-    dimension_20 = NeuralNetwork( save_weights=True, epochs=250,
-                                 hidden_size=400, hidden_amount=16, output_size=output_size, class_type=class_type, loss=loss, save_space=True, save_architecture=True, file_name="All-Layer-standard" + str(16))
+                                 hidden_size=[400], hidden_amount=1, output_size=output_size, class_type=class_type, loss=loss, save_space=True, save_architecture=True, file_name="All-Layer-400-L")
 
-    print "#### ALL ####"
-
-    print "standard:", dimension_200.objective_score, dimension_200.f1_score, dimension_200.correct_classes
-    print "Neural Network 0.02:", dimension_100.objective_score, dimension_100.f1_score, dimension_100.correct_classes
-    print "Neural Network 0.04:", dimension_50.objective_score, dimension_50.f1_score, dimension_50.correct_classes
-    print "Neural Network 0.06:", dimension_20.objective_score, dimension_20.f1_score, dimension_20.correct_classes
-
-    ifile = open('experiments/' + "All-Layer-standard" + '.csv', "w")
-    writer = csv.writer(ifile, delimiter=b" ")
-    writer.writerow("Hidden Size, Objective Score, Accuracy, F1, Correct Classes")
-    writer.writerow("2,", str(dimension_200.objective_score[0])+","+str(dimension_200.objective_score[1])+","+str(dimension_200.f1_score)+","+str(dimension_200.correct_classes)+"")
-    writer.writerow("4,"+str(dimension_100.objective_score[0])+","+str(dimension_100.objective_score[1])+","+str(dimension_100.f1_score)+","+str(dimension_100.correct_classes)+"")
-    writer.writerow("8,"+str(dimension_50.objective_score[0])+","+str(dimension_50.objective_score[1])+","+str(dimension_50.f1_score)+","+str(dimension_50.correct_classes)+"")
-    writer.writerow("16,"+str(dimension_20.objective_score[0])+","+str(dimension_20.objective_score[1])+","+str(dimension_20.f1_score)+","+str(dimension_20.correct_classes)+"")
-    ifile.close()
-    """
+    print "GENRES"
     class_type = "Genres"
     output_size = 25
     loss = "bias_binary_crossentropy"
 
-    dimension_200 = NeuralNetwork(save_weights=True, epochs=250,
-                                 hidden_size=400, hidden_amount=2, output_size=output_size, class_type=class_type, loss=loss, save_space=True, save_architecture=True, file_name="Genre-Layer-standard" + str(2))
     dimension_100 = NeuralNetwork(save_weights=True, epochs=250,
-                                 hidden_size=400, hidden_amount=4, output_size=output_size, class_type=class_type, loss=loss, save_space=True, save_architecture=True, file_name="Genre-Layer-standard" + str(4))
-    dimension_50 = NeuralNetwork( save_weights=True, epochs=250,
-                                 hidden_size=400, hidden_amount=8, output_size=output_size, class_type=class_type, loss=loss, save_space=True, save_architecture=True, file_name="Genre-Layer-standard" + str(8))
-    dimension_20 = NeuralNetwork( save_weights=True, epochs=250,
-                                 hidden_size=400, hidden_amount=10, output_size=output_size, class_type=class_type, loss=loss, save_space=True, save_architecture=True, file_name="Genre-Layer-standard" + str(10))
+                                 hidden_size=[400], hidden_amount=1, output_size=output_size, class_type=class_type, loss=loss, save_space=True, save_architecture=True, file_name="Genres-Layer-400-L")
 
-    print "#### GENRES ####"
-    print "Neural Network 0.005:", dimension_200.objective_score, dimension_200.f1_score, dimension_200.correct_classes
-    print "Neural Network 0.02:", dimension_100.objective_score, dimension_100.f1_score, dimension_100.correct_classes
-    print "Neural Network 0.04:", dimension_50.objective_score, dimension_50.f1_score, dimension_50.correct_classes
-    print "Neural Network 0.06:", dimension_20.objective_score, dimension_20.f1_score, dimension_20.correct_classes
-
-
-    columns = ["Hidden Size", "Objective Score", "Accuracy", "F1", "Correct Classes"]
-    data = ["2", dimension_200.objective_score[0], dimension_200.objective_score[1], dimension_200.f1_score, dimension_200.correct_classes]
-    df = pandas.DataFrame(data, columns=columns)
-    df.tocsv("experiments/Genres-Layer-standard.csv")
-
+    print "KEYWORDS"
     class_type = "Keywords"
     output_size = 100
     loss = "sparse_bias_binary_crossentropy"
 
-    dimension_200 = NeuralNetwork(save_weights=True, epochs=250,
-                                 hidden_size=400, hidden_amount=2, output_size=output_size, class_type=class_type, loss=loss, save_space=True, save_architecture=True, file_name="Keyword-Layer-standard" + str(2))
     dimension_100 = NeuralNetwork(save_weights=True, epochs=250,
-                                 hidden_size=400, hidden_amount=4, output_size=output_size, class_type=class_type, loss=loss, save_space=True, save_architecture=True, file_name="Keyword-Layer-standard" + str(4))
-    dimension_50 = NeuralNetwork( save_weights=True, epochs=250,
-                                 hidden_size=400, hidden_amount=8, output_size=output_size, class_type=class_type, loss=loss, save_space=True, save_architecture=True, file_name="Keyword-Layer-standard" + str(8))
-    dimension_20 = NeuralNetwork( save_weights=True, epochs=250,
-                                 hidden_size=400, hidden_amount=10, output_size=output_size, class_type=class_type, loss=loss, save_space=True, save_architecture=True, file_name="Keyword-Layer-standard" + str(10))
-
-    print "#### KEYWORDS ####"
-    print "Neural Network 0.005:", dimension_200.objective_score, dimension_200.f1_score, dimension_200.correct_classes
-    print "Neural Network 0.02:", dimension_100.objective_score, dimension_100.f1_score, dimension_100.correct_classes
-    print "Neural Network 0.04:", dimension_50.objective_score, dimension_50.f1_score, dimension_50.correct_classes
-    print "Neural Network 0.06:", dimension_20.objective_score, dimension_20.f1_score, dimension_20.correct_classes
-
-
-    columns = ["Hidden Size", "Objective Score", "Accuracy", "F1", "Correct Classes"]
-    data = ["2", dimension_200.objective_score[0], dimension_200.objective_score[1], dimension_200.f1_score, dimension_200.correct_classes]
-    df = pandas.DataFrame(data, columns=columns)
-    df.tocsv("experiments/Keywords-Layer-standard.csv")
-
+                                 hidden_size=[400], hidden_amount=1, output_size=output_size, class_type=class_type, loss=loss, save_space=True, save_architecture=True, file_name="Keywords-Layer-400-L")
 
 
 if  __name__ =='__main__':main()
